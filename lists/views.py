d2474e10ee1d11f9ee0dc2af346cffba0993077d
martin <martin.nobis@gmail.com>
2017-11-25 11:09:33 +1030

    new URL, view and template to display lists

--- lists/views.py
+++ lists/views.py
@@ -5,12 +5,15 @@ from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from lists.models import Item
 
+def view_list(request):
+	items = Item.objects.all()
+	return render(request, 'list.html', {'items': items})
+
 # Create your views here.
 def home_page(request):
 	if request.method == 'POST':
-		new_item_text = request.POST['item_text']
 		Item.objects.create(text=new_item_text) #MN: Don't need to call save() with create.
-		return redirect('/')
+		return redirect('/lists/the-only-list-in-the-world/')
 
 	items = Item.objects.all()
 	return render(request, 'home.html', {'items': items})
