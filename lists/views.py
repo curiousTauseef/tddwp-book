72729c75b1ed6a3819a0ecb57962bfda2c8f3ab7
Martin Nobis <martin.nobis@gmail.com>
2017-11-29 22:12:41 +1100

    Made change to database. Fixed broken lists/ tests.

--- lists/views.py
+++ lists/views.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.http import HttpResponse
 
 from django.shortcuts import redirect, render
-from lists.models import Item
+from lists.models import Item, List
 
 def view_list(request):
-	items = Item.objects.all()
-	return render(request, 'list.html', {'items': items})
+    items = Item.objects.all()
+    return render(request, 'list.html', {'items': items})
 
 # Create your views here.
 def home_page(request):
-	return render(request, 'home.html')
+    return render(request, 'home.html')
 
 def new_list(request):
-	Item.objects.create(text=request.POST['item_text'])
-	return redirect('/lists/the-only-list-in-the-world/')
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect('/lists/the-only-list-in-the-world/')
