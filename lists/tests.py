d2819ec292fbd1a34bccbaffc08543713a5bfdf5
martin <martin.nobis@gmail.com>
2017-11-29 18:57:38 +1030

    Switching to OSX, commiting changes.

--- lists/tests.py
+++ lists/tests.py
@@ -7,7 +7,7 @@ from django.http import HttpRequest
 from lists.views import home_page
 from django.template.loader import render_to_string
 
-from lists.models import Item #MN: for ItemModelTest
+from lists.models import Item, List #MN: for ItemModelTest
 
 class HomePageTest(TestCase):
 	
@@ -32,16 +32,24 @@ class NewListTest(TestCase):
 		response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
 		self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
 
 	def test_saving_and_retrieving_items(self):
+		list_ = List()
+		list_.save()
+
 		first_item = Item() #MN: A row in the database
 		first_item.text = 'The first (ever) list item' #MN: A column.
+		first_item.list = list_
 		first_item.save()
 
 		second_item = Item()
 		second_item.text = 'Item the second'
+		second_item.list = list_
 		second_item.save()
+
+		saved_list = List.objects.first()
+		self.assertEqual(saved_list, list_)
 
 		saved_items = Item.objects.all()
 		self.assertEqual(saved_items.count(), 2)
@@ -49,7 +57,9 @@ class ItemModelTest(TestCase):
 		first_saved_item = saved_items[0]
 		second_saved_item = saved_items[1]
 		self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+		self.assertEqual(first_saved_item.list, list_)
 		self.assertEqual(second_saved_item.text, 'Item the second')
+		self.assertEqual(second_saved_item.list, list_)
 
 class ListViewTest(TestCase):
 
